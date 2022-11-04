ee8c88952d28d4713b0ba7e25a531b24ce91265b
PinarAktas <pinaraktas81@gmail.com>
2022-11-04 11:46:27 -0400

    data.js is used for projects

---
+++ src/data.js
@@ -0,0 +1,100 @@
+export const frontEndPortfolio = [
+    {
+      id: 1,
+      title: "Professional Portfolio",
+      img:"assets/portfolio.png",
+      link:"https://pinaraktas.github.io/Pinar_Aktas_Portfolio/",
+    },
+    {
+      id: 2,
+      title: "Password Generator",
+      img:"assets/password.png",
+      link:"https://pinaraktas.github.io/Password_Generator_PA/"
+    },
+    {
+      id: 3,
+      title: "Quiz App",
+      img:"assets/quiz.png",
+      link:"https://pinaraktas.github.io/Quiz_App/",
+    },
+    {
+      id: 4,
+      title: "Work Day Scheduler",
+      img:"assets/scheduler.png",
+      link:"https://pinaraktas.github.io/Work_Day_Schedular/",
+    },
+    {
+      id: 5,
+      title: "Weather Dashboard",
+      img:"assets/weather.png",
+      link:"https://pinaraktas.github.io/Weather_app/",
+    },
+  ];
+  
+  export const backEndPortfolio = [
+    {
+      id: 1,
+      title: "Meal and Cocktail Recipes",
+      img:"assets/meal.png",
+      link:"https://jamshedsadullaev.github.io/Peculiar-terror/"
+    },
+    {
+      id: 2,
+      title: "README Generator",
+      img: "assets/readme.png",
+      link:"https://github.com/PinarAktas/ReadMe-Generator",
+    },
+    {
+      id: 3,
+      title: "Team Profile Generator",
+      img: "assets/team.png",
+      link:"https://github.com/PinarAktas/team_profile_generator",
+    },
+    {
+      id: 4,
+      title: "Employee Tracker",
+      img: "assets/employee.png",
+      link:"https://github.com/PinarAktas/Employee_Tracker"
+    },
+    {
+      id: 5,
+      title: "E-commerce Back End",
+      img: "assets/commerce.png",
+      link:"https://github.com/PinarAktas/E_Commerce-",
+    },
+    {
+      id: 6,
+      title: "Tech Blog",
+      img: "assets/blog.png",
+      link:"https://powerful-reef-21680.herokuapp.com/signup"
+    },
+  ];
+  
+  export const fullStackPortfolio = [
+    {
+      id: 1,
+      title: "Pet Instagram PawDate",
+      img:"assets/pawdate.png",
+      link:"https://agile-reef-42699.herokuapp.com/"
+    },
+    {
+      id: 2,
+      title: "Regex Tutorial",
+      img:"assets/regex.png",
+      link:"https://github.com/PinarAktas/Regex_Tutorial"
+    },
+    {
+      id: 3,
+      title: "Social Network API",
+      img:"assets/social.png",
+      link:"https://github.com/PinarAktas/Social_Network_API"
+    },
+    {
+      id: 4,
+      title: "React Portfolio",
+      img:"assets/reactportfolio.png",
+      link:"https://github.com/PinarAktas/React_Portfolio"
+    },
+  ];
+  
+ 
